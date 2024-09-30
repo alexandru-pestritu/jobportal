1b1c0fa1a0baaa3d2a8118145d59924dffa00b03
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-09-30 18:35:15 +0300

    Changed register method to create a new entry for user profile based on role

--- src/main/java/com/app/jobportal/services/UsersService.java
+++ src/main/java/com/app/jobportal/services/UsersService.java
@@ -1,6 +1,10 @@
 package com.app.jobportal.services;
 
+import com.app.jobportal.entity.JobSeekerProfile;
+import com.app.jobportal.entity.RecruiterProfile;
 import com.app.jobportal.entity.Users;
+import com.app.jobportal.repository.JobSeekerProfileRepository;
+import com.app.jobportal.repository.RecruiterProfileRepository;
 import com.app.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,16 +15,28 @@ import java.util.Optional;
 @Service
 public class UsersService {
     private final UsersRepository usersRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository, JobSeekerProfileRepository jobSeekerProfileRepository) {
         this.usersRepository = usersRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(users);
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        Users savedUser = usersRepository.save(users);
+
+        if(userTypeId == 1) {
+            recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        } else {
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
+        return savedUser;
     }
 
     public Optional<Users> getUserByEmail(String email) {
