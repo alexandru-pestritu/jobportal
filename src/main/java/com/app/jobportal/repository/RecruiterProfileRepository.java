598f6b378ea6508fa45292ea6341d0c999a0256e
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-09-30 18:34:46 +0300

    RecruiterProfile entity and repository

---
+++ src/main/java/com/app/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,8 @@
+package com.app.jobportal.repository;
+
+import com.app.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+}
