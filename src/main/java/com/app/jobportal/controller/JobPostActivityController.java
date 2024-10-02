0b959c5669de9c647b379089a6ed150326d6a5fe
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-10-02 17:26:31 +0300

    Add job functionality in JobPostActivityController

--- src/main/java/com/app/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/app/jobportal/controller/JobPostActivityController.java
@@ -1,22 +1,34 @@
 package com.app.jobportal.controller;
 
+import com.app.jobportal.entity.JobPostActivity;
+import com.app.jobportal.entity.RecruiterJobsDto;
+import com.app.jobportal.entity.RecruiterProfile;
+import com.app.jobportal.entity.Users;
+import com.app.jobportal.services.JobPostActivityService;
 import com.app.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
 
     private final UsersService usersService;
+    private final JobPostActivityService jobPostActivityService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService) {
         this.usersService = usersService;
+        this.jobPostActivityService = jobPostActivityService;
     }
 
     @GetMapping("/dashboard/")
@@ -27,8 +39,34 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile)currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
         model.addAttribute("user", currentUserProfile);
         return "dashboard";
     }
+
+    @GetMapping("/dashboard/add")
+    public String addJobs(Model model) {
+        model.addAttribute("jobPostActivity", new JobPostActivity());
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+
+        return "add-jobs";
+    }
+
+    @PostMapping("/dashboard/addNew")
+    public String addNew(JobPostActivity jobPostActivity, Model model) {
+        Users user = usersService.getCurrentUser();
+        if(user!=null) {
+            jobPostActivity.setPostedById(user);
+        }
+        jobPostActivity.setPostedDate(new Date());
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
+        return "redirect:/dashboard/";
+    }
+
+
 }
