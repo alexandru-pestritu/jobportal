b694c74aedbf46d0ad2ea1a89c61eb4b0dd80591
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-10-02 17:25:39 +0300

    JobPostActivity repository and service

---
+++ src/main/java/com/app/jobportal/services/JobPostActivityService.java
@@ -0,0 +1,40 @@
+package com.app.jobportal.services;
+
+import com.app.jobportal.entity.*;
+import com.app.jobportal.repository.JobPostActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobPostActivityService {
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    @Autowired
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+        for(IRecruiterJobs rec : recruiterJobsDtos) {
+            JobLocation location = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany company = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title(), location, company));
+        }
+        return  recruiterJobsDtoList;
+    }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepository.findById(id).orElseThrow(() -> new RuntimeException("Job not found."));
+    }
+}
