7979fe6eeb07c8be3c8bb171d09d407db103273a
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-10-02 21:48:19 +0300

    Methods to get current recruiter/job seeker profile

--- src/main/java/com/app/jobportal/services/JobSeekerProfileService.java
+++ src/main/java/com/app/jobportal/services/JobSeekerProfileService.java
@@ -1,7 +1,12 @@
 package com.app.jobportal.services;
 
 import com.app.jobportal.entity.JobSeekerProfile;
+import com.app.jobportal.entity.Users;
 import com.app.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +14,11 @@ import java.util.Optional;
 @Service
 public class JobSeekerProfileService {
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final UsersService usersService;
 
-    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository, UsersService usersService) {
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.usersService = usersService;
     }
 
     public Optional<JobSeekerProfile> getOne(Integer id) {
@@ -20,5 +27,16 @@ public class JobSeekerProfileService {
 
     public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
         return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+
+    public JobSeekerProfile getCurrentSeekerProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users user = usersService.getUserByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not find user."));
+            Optional<JobSeekerProfile> seekerProfile = getOne(user.getUserId());
+            return seekerProfile.orElse(null);
+        }
+        return null;
     }
 }
